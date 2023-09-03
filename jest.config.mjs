01231bd3348fb1ff4e779ae9a86ae40c2b994be3
Damian Chojnacki <damianchojnacki@op.pl>
2023-09-03 12:10:34 +0200

    feature: generate coverage badge

--- jest.config.mjs
+++ jest.config.mjs
@@ -10,6 +10,7 @@ const createJestConfig = nextJest({
 const config = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
+    coverageReporters: ["json-summary"],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
