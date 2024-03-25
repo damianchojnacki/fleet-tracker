d5eb517a4f9e4bfea7eb88f80b65da2d464fafa7
Damian Chojnacki <daamian3@gmail.com>
2024-03-25 11:35:05 +0100

    fix: primary color

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -8,7 +8,7 @@ export function UserAvatar({ initials }) {
   return (
     <Avatar className="mx-3">
       <AvatarImage src="https://github.com/shadcn.png" />
-      <AvatarFallback className="bg-[#1B4865] text-white">{initials}</AvatarFallback>
+      <AvatarFallback className="bg-primary text-white">{initials}</AvatarFallback>
     </Avatar>
   )
 }
